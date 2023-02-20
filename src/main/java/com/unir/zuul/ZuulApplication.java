b9c28ac561467f1b3f5647b57c49deeeb11e75e2
Pablo-Chandi <pachandic@gmail.com>
2023-02-19 23:40:49 -0500

    fix cors

--- src/main/java/com/unir/zuul/ZuulApplication.java
+++ src/main/java/com/unir/zuul/ZuulApplication.java
@@ -3,6 +3,9 @@ package com.unir.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +15,16 @@ public class ZuulApplication {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
